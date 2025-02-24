8a0d28b776eeeef37e476f1ec99c3e061c855655
Zacks_World <elguazzarzakaria10@gmail.com>
2025-02-24 16:40:04 +0000

    Modify and Corection of  config.txt & Add dynamic instantiation of MetierImpl in PresentationV2

--- src/main/java/Presentation/PresentationV2.java
+++ src/main/java/Presentation/PresentationV2.java
@@ -1,6 +1,7 @@
 package Presentation;
 
 import Dao.IDao;
+import Metier.IMetier;
 
 import java.io.File;
 import java.util.Scanner;
@@ -9,10 +10,16 @@ public class PresentationV2 {
     public static void main(String[] args) {
         try {
             Scanner scanner = new Scanner(new File("config.txt"));
+            //DaoImpl dao = new DaoImpl();
             String daoClassname = scanner.nextLine();
             Class<?> cDao = Class.forName(daoClassname);
             IDao dao = (IDao) cDao.getConstructor().newInstance();
-            System.out.println(dao.getData());
+            //MetierImpl metier = new MetierImpl(dao);
+            String metierClassname = scanner.nextLine();
+            Class<?> cMetier = Class.forName(metierClassname);
+            IMetier metier = (IMetier) cMetier.getConstructor(IDao.class).newInstance(dao);
+            System.out.println("RES="+metier.calcul());
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
