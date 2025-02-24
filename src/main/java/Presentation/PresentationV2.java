6b8ce6ff8afd7250c4b385c45e299a100f3dfb32
Zacks_World <elguazzarzakaria10@gmail.com>
2025-02-24 16:43:22 +0000

    Adding the dynamic instantiation of MetierImpl in PresentationV2 via setter

--- src/main/java/Presentation/PresentationV2.java
+++ src/main/java/Presentation/PresentationV2.java
@@ -4,6 +4,7 @@ import Dao.IDao;
 import Metier.IMetier;
 
 import java.io.File;
+import java.lang.reflect.Method;
 import java.util.Scanner;
 
 public class PresentationV2 {
@@ -17,7 +18,10 @@ public class PresentationV2 {
             //MetierImpl metier = new MetierImpl(dao);
             String metierClassname = scanner.nextLine();
             Class<?> cMetier = Class.forName(metierClassname);
-            IMetier metier = (IMetier) cMetier.getConstructor(IDao.class).newInstance(dao);
+            IMetier metier = (IMetier) cMetier.getConstructor().newInstance();
+            //metier.setDao(dao);
+            Method setDao= cMetier.getDeclaredMethod("setDao", IDao.class);
+            setDao.invoke(metier, dao);
             System.out.println("RES="+metier.calcul());
 
         } catch (Exception e) {
