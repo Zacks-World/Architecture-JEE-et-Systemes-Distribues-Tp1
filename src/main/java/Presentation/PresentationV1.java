07b09935e0efa4a5757b738d580fbdb213c52d24
Zacks_World <elguazzarzakaria10@gmail.com>
2025-02-24 15:54:53 +0000

    Modify the type of the object dao from DaoImpl to DaoImplV2

--- src/main/java/Presentation/PresentationV1.java
+++ src/main/java/Presentation/PresentationV1.java
@@ -2,13 +2,14 @@ package Presentation;
 
 import Dao.DaoImpl;
 import Metier.MetierImpl;
+import ext.DaoImplV2;
 
 public class PresentationV1 {
     public static void main(String[] args) {
         /*
         Injection des dependances par instanciation statique => new
          */
-        DaoImpl dao = new DaoImpl();
+        DaoImplV2 dao = new DaoImplV2();
         MetierImpl metier = new MetierImpl(dao);// Injection via le constructeur
         metier.setDao(dao); // Injection via le setter
         System.out.println("RES="+metier.calcul());
